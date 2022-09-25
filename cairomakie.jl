7dc6a03779185cf9d391f715cdd6f6e44d07930b
Maximilian Köhler <maximilian.koehler@ruhr-uni-bochum.de>
2022-09-25 23:30:35 +0200

    finalize cairomakie example

--- cairomakie.jl
+++ cairomakie.jl
@@ -1,37 +1,37 @@
-using Ferrite, FerriteViz#, FerriteGmsh
+using Ferrite, FerriteViz, FerriteGmsh, Colors
 import CairoMakie
 CairoMakie.activate!()
 
-#ferritelogo = saved_file_to_grid("ferrite-logo.msh")
-#dh_logo = DofHandler(ferritelogo)
-#push!(dh_logo,:u,1)
-#close!(dh_logo)
-#u_logo = zeros(ndofs(dh_logo))
-#julia_purple=1
-#julia_red=2
-#julia_blue=3
-#julia_green=4
-#colormap = Dict(
-#    "1" => julia_purple,
-#    "2" => julia_red,
-#    "3" => julia_red,
-#    "4" => julia_blue,
-#    "5" => julia_purple,
-#    "6" => julia_green
-#)
-#cellset_u = zeros(getncells(ferritelogo))
-#for (cellidx,cell) in enumerate(getcells(ferritelogo))
-#    for (cellsetname,cellsetvalue) in colormap
-#        if cellidx in ferritelogo.cellsets[cellsetname]
-#            cellset_u[cellidx] = cellsetvalue
-#        end
-#    end
-#end
-#plotterlogo = MakiePlotter(dh_logo,u_logo)
-#
-#fig_logo, ax_logo, cp_logo = cellplot(plotterlogo,cellset_u,colormap=[colorant"rgb(149,88,178)",colorant"rgb(203,60,51)",colorant"rgb(64,99,216)",colorant"rgb(56,152,38)"])
-#wireframe!(plotterlogo,markersize=0,strokewidth=2)
-#CairoMakie.save("ferrite-logo.pdf",fig_logo)
+ferritelogo = saved_file_to_grid("ferrite-logo.msh")
+dh_logo = DofHandler(ferritelogo)
+push!(dh_logo,:u,1)
+close!(dh_logo)
+u_logo = zeros(ndofs(dh_logo))
+julia_purple=1
+julia_red=2
+julia_blue=3
+julia_green=4
+colormap = Dict(
+    "1" => julia_purple,
+    "2" => julia_red,
+    "3" => julia_red,
+    "4" => julia_blue,
+    "5" => julia_purple,
+    "6" => julia_green
+)
+cellset_u = zeros(getncells(ferritelogo))
+for (cellidx,cell) in enumerate(getcells(ferritelogo))
+    for (cellsetname,cellsetvalue) in colormap
+        if cellidx in ferritelogo.cellsets[cellsetname]
+            cellset_u[cellidx] = cellsetvalue
+        end
+    end
+end
+plotterlogo = MakiePlotter(dh_logo,u_logo)
+
+fig_logo, ax_logo, cp_logo = cellplot(plotterlogo,cellset_u,colormap=[colorant"rgb(149,88,178)",colorant"rgb(203,60,51)",colorant"rgb(64,99,216)",colorant"rgb(56,152,38)"])
+wireframe!(plotterlogo,markersize=0,strokewidth=2)
+CairoMakie.save("ferrite-logo.pdf",fig_logo)
 
 include("heat-equation.jl")
 surface(dh,u)
